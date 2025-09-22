1db1c90c7fcedeb1c3a04cb93c257d5b41040b07
anika1679 <anika.khatri@gmail.com>
2025-09-22 14:36:01 -0400

    Made the competitor data into a json

--- google-cloud-demo/src/config.py
+++ google-cloud-demo/src/config.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
-GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+# Gets the environment variables from the env file and configures them
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 PROJECT_ID = os.getenv("PROJECT_ID")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- google-cloud-demo/src/wrapper.py
+++ google-cloud-demo/src/wrapper.py
@@ -1,17 +1,52 @@
 from google import genai
 import os
-from .config import GEMINI_API_KEY, GOOGLE_API_KEY, GOOGLE_APPLICATION_CREDENTIALS, PROJECT_ID
+from .config import GEMINI_API_KEY, GOOGLE_API_KEY, PROJECT_ID
+import json
+import re
+from pptx import Presentation
+from pptx.util import Inches
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
-
+# Initializes the ai client being used.
 client = genai.Client(vertexai=True, project=PROJECT_ID, location="global")
-
-val = input("Enter the names of your competitors:")
-
-prompt = f"""Think of yourself as a business analyst for a company. Create a side-by-side competitve benchmark analysis for these competitors: {val}
-For each competitor be sure to list their strengths, weaknesses, opportunities, and risks. Format everything in a clear and readable table"""
-
+# Gets the information of the company and its compeitors from the user
+company_name = input("Name of your company:")
+description = input("Please give a description of your companies operations:")
+competitors = input("Enter the names of your competitors:")
+# Prompt to be used by the AI client
+prompt = f"""Think of yourself as a business analyst for {company_name}. Their description is: {description}. Create a side-by-side competitve benchmark analysis for these competitors: {competitors}
+For each competitor be sure to list their strengths, weaknesses, opportunities, and risks. Format as JSON with competitor names as keys. """
+# Gets the response from the client
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents=prompt)
+response_text = response.text
 
-print(response.text, end="")
+# Creates the data from AI response into a json that can be parsed
+competitor_json = None
+json_text_match = re.search(r"\{.*\}", response_text, re.DOTALL)
+if json_text_match:
+    json_text = json_text_match.group(0)
+    try:
+        competitor_json=json.loads(json_text)
+        print("Parsed Successfully")
+    except json.JSONDecodeError as e:
+        print("Warning: Not valid JSON", e)
+
+print(competitor_json)
+
+if not competitor_json:
+    print("Error: could not parse data")
+    exit()
+
+# Second prompt to compare the competitors to the user's company
+prompt2 = f"""Think of yourself as a business analyst for {company_name}. Their description is: {description}.
+Start first with a company overview and their key stats. Then using this competitor data in JSON format: {competitor_json}
+For each competitor, give:
+- Overview and key stats
+- SWOT analysis
+- Strategic recommendations for {company_name}
+- Implications
+Format your output as JSON, with each topic as a new slide section. Make content concise and actionable"""
+
+def createPowerPoints():
+    prs = Presentation()
+
