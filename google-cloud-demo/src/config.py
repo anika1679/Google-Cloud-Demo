d03a24d728d0e6c9a7338f4bd59b5ccccdb39f86
anika1679 <anika.khatri@gmail.com>
2025-09-22 13:29:22 -0400

    Got the api to work

--- google-cloud-demo/src/config.py
+++ google-cloud-demo/src/config.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+PROJECT_ID = os.getenv("PROJECT_ID")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
--- google-cloud-demo/src/wrapper.py
+++ google-cloud-demo/src/wrapper.py
@@ -1,10 +1,17 @@
 from google import genai
+import os
+from .config import GEMINI_API_KEY, GOOGLE_API_KEY, GOOGLE_APPLICATION_CREDENTIALS, PROJECT_ID
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
+
+client = genai.Client(vertexai=True, project=PROJECT_ID, location="global")
 
 val = input("Enter the names of your competitors:")
 
-
-client = genai.Client()
+prompt = f"""Think of yourself as a business analyst for a company. Create a side-by-side competitve benchmark analysis for these competitors: {val}
+For each competitor be sure to list their strengths, weaknesses, opportunities, and risks. Format everything in a clear and readable table"""
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents=""
-)
+    model="gemini-2.5-flash", contents=prompt)
+
+print(response.text, end="")
